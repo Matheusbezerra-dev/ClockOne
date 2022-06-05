4484c5f19b7cc27a1a599486ae8576f54980f0a3
Matheus Bezerra <mabezerra2@gmail.com>
2022-06-05 11:19:05 -0400

    criando elementos e adicionando ids e class

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,24 @@ window.onload = function createStructure() {
 function createDivChild() {
   const div = document.getElementById('clock');
   const div1 = document.createElement('div');
-  div.appendChild(div1)
+  div.appendChild(div1);
   div1.setAttribute('id', 'hour');
-  
+  const div2 = document.createElement('div');
+  div.appendChild(div2);
+  div2.setAttribute('id', 'minutes');
+  const div3 = document.createElement('div')
+  div.appendChild(div3);
+  div3.setAttribute('id', 'second');  
+  numbersClock()
+}
+
+function numbersClock() {
+  const divcapture = document.getElementById('clock');
+  for (let i = 1; i <= 12; i += 1) {   
+    const divs = document.createElement('div')
+    divs.className = 'number';
+    divs.classList.add([i])
+    divs.innerText = [i]       
+    divcapture.appendChild(divs)
+  }
 }
